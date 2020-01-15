126af2d94dbc95dcb885f3701e9bb57625ae0ff2
wangwenhao1223 <894870731@qq.com>
2020-01-15 21:38:35 +0800

    1月15日提交的信信息

--- src/js/online.js
+++ src/js/online.js
@@ -1,11 +1,13 @@
-function Online() {
-    this.Lubotu = $('.online');
-    this.Ul = $('.online ul');
-    this.Li = $('.online ul li');
-    this.An = $('.online ol li');
-    //    this.num=0;
-    this.timer = null;
-}
+//给轮播图做点击事件
+class Lunbo{
+    constructor(){
+        this.pics=document.querySelectorAll('.on_pic li')//取得三张图片
+        this.btns=document.querySelectorAll('.on_an li')//取得三个按钮
+        this.arrowright=document.querySelector('#right');
+        this.arrowleft=document.querySelector('#left');
+        this.index=0;
+        this.timer=null;
+    }
 Online.prototype.init=function(){
    var zhege=this;
 }
